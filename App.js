a43dd227dc41db4d25c861334a150e1ef8500df3
manashvi07032006 <manashvi.singh.irwo@gmail.com>
2021-11-18 14:54:31 +0530

    my commit

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from './screens/Home';
+import StarMapScreen from './screens/StarMap';
+import SpaceCraftScreen from './screens/SpaceCraft';
+import DailyPicsScreen from './screens/DailyPic';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigator 
+      <Stack.Navigator 
         initialRouteName="Home" 
         screenOptions={{headerShown:false}} >
           <Stack.Screen name="Home" component={HomeScreen}/>
-      </StackNavigator>
+          <Stack.Screen name="StarMap" component={StarMapScreen}/>
+          <Stack.Screen name="SpaceCraft" component={SpaceCraftScreen}/>
+          <Stack.Screen name="DailyPic" component={DailyPicsScreen}/>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
